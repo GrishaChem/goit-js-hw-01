e087d44c5da3753eb0ca05ed49bffa7f683451e6
GrishaChem <Hryhoryy.chemerys@gmail.com>
2024-05-21 19:52:01 +0300

    asdad

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
 function getElementWidth(content, padding, border) {
-  [padding, border, content] = [
-    parseInt(padding),
-    parseInt(border),
-    parseInt(content),
-  ];
-  return content + 2 * padding + 2 * border;
+  let contentValue = parseFloat(content);
+  let paddingValue = parseFloat(padding);
+  let borderValue = parseFloat(border);
+
+  return contentValue + 2 * paddingValue + 2 * borderValue;
 }
+
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
